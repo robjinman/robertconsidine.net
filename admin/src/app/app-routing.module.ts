685d02e46f3433a2e50510cb06fdedd50422d6d3
Rob Jinman <jinmanr@gmail.com>
2019-02-03 19:09:55 +0000

    Added some more components for the various routes

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CommentsComponent } from './comments/comments.component';
+import { SubscribersComponent } from './subscribers/subscribers.component';
+import { ComposeComponent } from './compose/compose.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'comments', component: CommentsComponent },
+  { path: 'subscribers', component: SubscribersComponent },
+  { path: 'compose', component: ComposeComponent }
 ];
 
 @NgModule({
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ComposeComponent } from './compose/compose.component';
+import { SubscribersComponent } from './subscribers/subscribers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ArticlesComponent,
+    CommentsComponent,
+    ComposeComponent,
+    SubscribersComponent
   ],
   imports: [
     BrowserModule,
